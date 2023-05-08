6909fd2cc6076afb450b910e0d7e7b39a4077020
lynnzammit22 <lynn.zammit.22@um.edu.mt>
2023-05-08 19:17:20 +0200

    feat("): image updates

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
         animation: 'random',
         animationDuration: 20000,
         slides: [{
-            src: './images/portfolio/Photos/34.JPG'
+            src: './images/portfolio/photos/34.JPG'
         }]
     });
 
