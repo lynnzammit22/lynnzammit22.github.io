7cace250ec12c49f0430ddeac0ad5137b52aa478
lynnzammit22 <lynn.zammit.22@um.edu.mt>
2023-05-12 00:19:11 +0200

    feat("): final site

--- js/main.js
+++ js/main.js
@@ -1,76 +1,77 @@
+function updateActiveNavItem() {
+  const hash = window.location.hash;
+  $('.nav-item.active').removeClass('active');
+  $(`a.nav-link[href="${hash}"]`).closest('.nav-item').addClass('active');
+  $('.navbar-toggler').attr('aria-expanded', 'false').toggleClass('collapsed', true); 
+  $('.navbar-collapse').collapse('hide'); 
+}
+
+$(window).on('hashchange', updateActiveNavItem);
+
 $(document).ready(function() {
 
-    /* Scroll hire me button to contact page */
-    $('.hire-me').click(function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-        return false;
+  updateActiveNavItem();
+
+  /* For Bootstrap current state on portfolio sorting */
+  $('ul.nav-pills li').on('click', function() {
+    $('ul.nav-pills li.active').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /* portfolio mixitup */
+  $("#grid").isotope({
+    filter: function() {
+      const name = $('.grid-wrapper .name').text();
+      return name.endsWith('ium');
+    }
+  })
+
+  /* welcome page */
+  $('#page-welcome').vegas({
+    overlay: true,
+    transition: 'fade',
+    transitionDuration: 4000,
+    delay: 10000,
+    animation: 'random',
+    animationDuration: 20000,
+    slides: [{
+      src: './images/portfolio/photos/34.JPG'
+    }]
+  });
+
+  /* isotope filtering */
+  $(window).on('load', function() {
+    const $container = $('.grid-wrapper');
+    $container.isotope({
+      filter: '*',
+      masonry: {
+        isFitWidth: true
+      }
     });
 
-    /* For Bootstrap current state on portfolio sorting */
-    $('ul.nav-pills li').on('click', function() {
-        $('ul.nav-pills li.active').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    /* portfolio mixitup */
-    $("#grid").isotope({
-        filter: function() {
-            var name = $('.grid-wrapper').find('.name').text();
-            return name.match(/ium$/);
+    $('.grid-controls li a').on('click', function(event) {
+      event.preventDefault();
+      $('.grid-controls .current').removeClass('current');
+      $(this).addClass('current');
+      const selector = $(this).attr('data-filter');
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
         }
-    })
-
-    /* welcome page */
-    $('#page-welcome').vegas({
-        overlay: true,
-        transition: 'fade',
-        transitionDuration: 4000,
-        delay: 10000,
-        animation: 'random',
-        animationDuration: 20000,
-        slides: [{
-            src: './images/portfolio/photos/34.JPG'
-        }]
+      });
     });
+  });
 
-
-    /* isotope filtering */
-    $(window).on('load', function() {
-        var $container = $('.grid-wrapper');
-        $container.isotope({
-            filter: '*',
-            masonry: {
-                isFitWidth: true
-            }
-        });
-
-        $('.grid-controls li a').on('click', function() {
-            $('.grid-controls .current').removeClass('current');
-            $(this).addClass('current');
-
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-    });
-
-
-    /* Magnific Popup */
-    $('.grid-wrapper').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
+  /* Magnific Popup */
+  $('.grid-wrapper').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
 
 });
